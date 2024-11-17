cbec7b270a519d18ffd1d6540a661bc870d4d0f7
mk-2-56 <hal.takahashi.honoka.2025@gmail.com>
2024-11-17 18:02:33 +0900

    コインパッケージ入れ

---
+++ Assets/Scripts/Gimmick/Coin/CoinController.cs
@@ -0,0 +1,47 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.VFX;
+
+
+public class CoinController : MonoBehaviour
+{
+    [SerializeField] float RotSpeed;                // アイドル状態の回転アニメ速度
+
+    private VisualEffect coinEffect;
+    private AudioSource coinSound;
+    private bool isTouched;
+    private Vector3 Axe = Vector3.up;
+
+    void Start()
+    {
+        coinSound = GetComponent<AudioSource>();
+        coinEffect = GetComponent<VisualEffect>();
+    }
+
+    void Update()
+    {
+        // 回転させておく
+        if(!isTouched) transform.RotateAround(transform.localPosition, Axe, RotSpeed);
+    }
+
+    private void OnTriggerEnter()
+    {
+        // 一旦非表示に
+        Destroy(GetComponent<MeshRenderer>());
+        Destroy(GetComponent<BoxCollider>());
+
+        // スコアに加算 あとでUImanagerとかからやるようにしたい
+        // coinScore.AddCoin();
+
+        // エフェクトと音を再生
+        coinEffect.SendEvent("EventCoin");
+        if (coinSound != null) coinSound.PlayOneShot(coinSound.clip);
+
+        // 音が鳴り終わったらコインは消す
+        Destroy(gameObject, coinSound.clip.length);
+    }
+
+}
