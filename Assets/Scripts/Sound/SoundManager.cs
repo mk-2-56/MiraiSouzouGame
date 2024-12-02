ea798072ed5e90d8ed3119e6dd1442c889151873
isaogithub <isaolin0903@gmail.com>
2024-12-03 00:56:41 +0900

    SoundManager修正

--- Assets/Scripts/Sound/SoundManager.cs
+++ Assets/Scripts/Sound/SoundManager.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
-    [SerializeField] AudioSource curBgmAudioSource;
-    [SerializeField] AudioSource curSeAudioSource;
+    [SerializeField] private AudioSource curBgmAudioSource;
+    [SerializeField] private AudioSource curSeAudioSource;
 
     [SerializeField] List<BGMSoundData> bgmSoundDatas;
     [SerializeField] List<SESoundData> seSoundDatas;
@@ -40,12 +40,12 @@ public class SoundManager : MonoBehaviour
 
     private void Update()
     {
-        for(int i = 0; i < bgmSoundDatas.Count; i++)
+        for (int i = 0; i < bgmSoundDatas.Count; i++)
         {
             curBgmAudioSource.volume = bgmSoundDatas[i].volume * seMasterVolume * masterVolume;
         }
 
-        for(int i = 0;i < seSoundDatas.Count; i++)
+        for (int i = 0; i < seSoundDatas.Count; i++)
         {
 
         }
@@ -62,7 +62,7 @@ public class SoundManager : MonoBehaviour
     {
         BGMSoundData data = bgmSoundDatas.Find(data => data.bgm == bgmNum);
 
-        if(data == null)
+        if (data == null)
         {
             data = new BGMSoundData();
             data.bgm = bgmNum;
@@ -76,6 +76,24 @@ public class SoundManager : MonoBehaviour
         curBgmAudioSource.Play();
     }
 
+    public void PlayBGM(BGMSoundData.BGM bgmType)
+    {
+        // 対応するBGMデータを取得
+        BGMSoundData bgmData = bgmSoundDatas.Find(data => data.bgm == bgmType);
+
+        if (bgmData != null)
+        {
+            // AudioClipと音量を設定して再生
+            curBgmAudioSource.clip = bgmData.audioClip;
+            curBgmAudioSource.volume = bgmData.volume * bgmMasterVolume * masterVolume;
+            curBgmAudioSource.Play();
+        }
+        else
+        {
+            Debug.LogWarning($"指定されたBGM ({bgmType}) が見つかりません。");
+        }
+    }
+
     public void PlaySE(AudioSource seSource)
     {
         curSeAudioSource = seSource;
@@ -83,7 +101,7 @@ public class SoundManager : MonoBehaviour
         curSeAudioSource.PlayOneShot(seSource.clip);
     }
 
-    public void PlaySE(AudioSource seSource,SESoundData.SE seNum)
+    public void PlaySE(AudioSource seSource, SESoundData.SE seNum)
     {
         SESoundData data = seSoundDatas.Find(data => data.se == seNum);
 
@@ -95,12 +113,65 @@ public class SoundManager : MonoBehaviour
             data.volume = seSource.volume;
             seSoundDatas.Add(data);
         }
-        
+
         curSeAudioSource = seSource;
         curSeAudioSource.volume = data.volume * seMasterVolume * masterVolume;
         curSeAudioSource.PlayOneShot(data.audioClip);
     }
 
+    public void PlaySE(SESoundData.SE seType)
+    {
+        // 対応するBGMデータを取得
+        SESoundData seData = seSoundDatas.Find(data => data.se == seType);
+
+        if (seData != null)
+        {
+            // AudioClipと音量を設定して再生
+            curSeAudioSource.clip = seData.audioClip;
+            curSeAudioSource.volume = seData.volume * seMasterVolume * masterVolume;
+            curSeAudioSource.Play();
+        }
+        else
+        {
+            Debug.LogWarning($"指定されたSE ({seType}) が見つかりません。");
+        }
+    }
+
+    public void SetMasterVolume(float volume)
+    {
+        masterVolume = Mathf.Clamp01(volume);
+        UpdateVolumes();
+    }
+    public void SetBGMVolume(float volume)
+    {
+        bgmMasterVolume = Mathf.Clamp01(volume);
+        UpdateVolumes();
+    }
+
+    public void SetSEVolume(float volume)
+    {
+        seMasterVolume = Mathf.Clamp01(volume);
+        UpdateVolumes();
+    }
+
+    private void UpdateVolumes()
+    {
+        // 現在再生中のBGMの音量を更新
+        if (curBgmAudioSource != null && curBgmAudioSource.isPlaying)
+        {
+            BGMSoundData currentBGM = bgmSoundDatas.Find(data => data.audioClip == curBgmAudioSource.clip);
+            if (currentBGM != null)
+            {
+                curBgmAudioSource.volume = currentBGM.volume * bgmMasterVolume * masterVolume;
+            }
+        }
+
+        // 効果音用AudioSourceの音量も更新（必要なら複数対応）
+        if (curSeAudioSource != null)
+        {
+            curSeAudioSource.volume = seMasterVolume * masterVolume;
+        }
+    }
 }
 
 [System.Serializable]
@@ -126,7 +197,12 @@ public class SESoundData
 {
     public enum SE
     {
+        SE_Click,
+        SE_Select,
+        SE_Cancel,
         SE_Button,
+        SE_SceneSwith,
+        SE_SceneLoading,
         SE_COIN,
         SE_Metour,
         SE_Pubble,
