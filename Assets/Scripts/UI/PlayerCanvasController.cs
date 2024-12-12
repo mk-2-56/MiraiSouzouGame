25cbec2cbd73ae109d731d9343db0ec5f590b6c0
isaogithub <isaolin0903@gmail.com>
2024-12-12 17:52:59 +0900

    速度メーターを数字にしてみた

--- Assets/Scripts/UI/PlayerCanvasController.cs
+++ Assets/Scripts/UI/PlayerCanvasController.cs
@@ -2,15 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using static Unity.Burst.Intrinsics.X86.Avx;
-
+using TMPro;
 public class PlayerCanvasController : MonoBehaviour
 {
     public CoinGaugeController _gaugeController;
     public CoinCollector       _coinCollector;
     public GameObject Canvas { get { return _canvas; } set { _canvas = value; } }
+
+    [SerializeField] float param_minSpeed = 40f;
+    [SerializeField] float param_maxSpeed = 100f;
+    [SerializeField] Color _startColor = Color.white;
+    [SerializeField] Color _middleColor = Color.yellow;
+    [SerializeField] Color _endColor = Color.red;
     private GameObject _canvas;
     private CoinGaugeController gaugeController;
-
+    private TextMeshProUGUI textMeshPro;
     public void Initialized()
     {
         if (Canvas == null) {
@@ -33,7 +39,10 @@ public class PlayerCanvasController : MonoBehaviour
         Canvas canvasComponent = _canvas.GetComponent<Canvas>();
         canvasComponent.renderMode = RenderMode.ScreenSpaceCamera;
 
-        gaugeController = _canvas.transform.Find("gauge_red").GetComponent<CoinGaugeController>();       
+        gaugeController = _canvas.transform.Find("gauge_red").GetComponent<CoinGaugeController>();
+        textMeshPro = _canvas.transform.Find("SpeedText").GetComponent<TextMeshProUGUI>();
+
+        this.transform.Find("Facing/Cog/EffectDispatcher").GetComponent<PlayerEffectDispatcher>().SpeedE += SetSpeedText;
     }
 
     private void Update()
@@ -42,6 +51,21 @@ public class PlayerCanvasController : MonoBehaviour
         {
             AddGaugeVaule(-0.01f);
         }*/
+
+        
+    }
+
+    public void SetSpeedText(float speed)
+    {
+        textMeshPro.text = speed.ToString("F0");
+        float range = param_maxSpeed - param_minSpeed;
+        float d = (speed - param_minSpeed);
+        float k = Mathf.Clamp(d / range, 0, 1);
+
+        float intensity = 3 * k * k - 2 * k * k * k;
+        intensity *= 2;
+        if (intensity > 1) {textMeshPro.color = Color.Lerp(_middleColor, _endColor, intensity - 1f);}
+        else { { textMeshPro.color = Color.Lerp(_startColor, _middleColor, intensity); } }        
     }
 
     public float GetGaugeValue()
