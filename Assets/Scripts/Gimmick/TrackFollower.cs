6ce4e5a324092475773394fcb4cc5c72aaca3781
isaogithub <isaolin0903@gmail.com>
2024-12-19 15:29:25 +0900

    大ジャンプ終わりの着陸自然になった

--- Assets/Scripts/Gimmick/TrackFollower.cs
+++ Assets/Scripts/Gimmick/TrackFollower.cs
@@ -111,14 +111,7 @@ public class TrackFollower : MonoBehaviour
     private void AddEndTrackVec(GameObject player, Vector3 direction)
     {
         float moveSpeed = trackSpeed; // 継続的な移動速度
-
-        // プレイヤーの向きを進行方向に合わせる
-        if (direction != Vector3.zero)
-        {
-            player.transform.rotation = Quaternion.LookRotation(direction);
-        }
-
         // プレイヤーを進行方向に移動
-        player.transform.position += direction * moveSpeed * Time.deltaTime;
+        player.GetComponent<Rigidbody>().velocity = direction * moveSpeed;
     }
 }
